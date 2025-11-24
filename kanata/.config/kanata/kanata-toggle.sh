86ebc0904c18ec945f024cc959676ba39c99412c
Aum <dontmailaum@tuta.io>
2025-11-24 11:12:26 +0530

    Added uwsm to hyprconfig

--- kanata/.config/kanata/kanata-toggle.sh
+++ kanata/.config/kanata/kanata-toggle.sh
@@ -4,7 +4,9 @@
 if pgrep -x "kanata" >/dev/null; then
   # If kanata  is running, kill it
   pkill -x kanata
+  notify-send -u normal -t 1500 "Killed Kanata"
 else
   # If wofi is not running, launch it
   kanata
+  notify-send -u normal -t 1500 "Launched Kanata"
 fi
